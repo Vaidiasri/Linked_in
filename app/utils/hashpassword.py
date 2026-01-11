0eec90e3d7199f17630c6591c8be245ad8a52f35
Vaibhav Ghildiyal <vaibhavghildiyal2101@gmail.com>
2026-01-11 22:43:10 +0530

    feat: implement JWT authentication for login endpoint

--- app/utils/hashpassword.py
+++ app/utils/hashpassword.py
@@ -1,9 +1,15 @@
 from pwdlib import PasswordHash
-# make  an instance 
-password_hashed= PasswordHash.recommended()
+from pwdlib.hashers.bcrypt import BcryptHasher
+
+# make  an instance with bcrypt hasher
+password_hashed = PasswordHash((BcryptHasher(),))
+
+
 # make  an function to hash the password
 def get_password_hash(password):
     return password_hashed.hash(password)
-# another function to verify the password 
+
+
+# another function to verify the password
 def verify_password(plain_password, hashed_password):
     return password_hashed.verify(plain_password, hashed_password)
